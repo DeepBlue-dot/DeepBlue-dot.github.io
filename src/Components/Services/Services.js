eb2b2431f25e1d4af1a3b77374b5455d17eb29d3
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2024-12-13 01:36:45 +0300

    completed services

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,43 @@
-import './Services.css'
+import "./Services.css";
+import { services } from "../../sources";
+import { Link } from "react-scroll";
 
 function Services() {
-    return (
-        <h1>
-            Services
-        </h1>
-    )
+  return (
+    <section id="services">
+      <div className="wrapper">
+        <div className="section-header">
+          <h1 className="heading-1">
+            <span className="gradient-text">Services</span>
+          </h1>
+          <h4 className="sub-title muted">
+            I transform your ideas, ans consequently your desires, into a
+            distinctive web project that both inspires you and captivates your
+            customers
+          </h4>
+        </div>
+        <div className="services-container">
+          {services.map((services, index) => {
+            return (
+              <div className="service" key={index}>
+                <div className="flex-center icon-wrapper">
+                    {services.icon}
+                </div>
+                <div className="details">
+                    <h3 className="name gradient-text">{services.name}</h3>
+                    <p className="mutted">{services.description}</p>
+                </div>
+                <div className="flex buttons-wrapper">
+                    <button className="btn">Read More</button>
+                    <Link to='contact' smooth={true} className="btn">Get started</Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
