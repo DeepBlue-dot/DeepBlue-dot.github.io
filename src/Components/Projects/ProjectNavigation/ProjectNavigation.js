410c9f39d121cd5516d9ea4ae961eae588231180
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2024-12-13 21:35:56 +0300

    completeed projectNavigation

---
+++ src/Components/Projects/ProjectNavigation/ProjectNavigation.js
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import "./ProjectNavigation.css";
+
+function ProjectNavigation({ tabs, onChange }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [indicatorWidth, setIndicatorWidth] = useState(0);
+  const itemsEls = useRef([]);
+  const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  };
+
+  useEffect(() => {
+    const prevEl = itemsEls.current.filter((_, index) => index < activeIndex);
+    setOffset(sumArray(prevEl.map((item) => item.offsetWidth)));
+    setIndicatorWidth(itemsEls.current[activeIndex].offsetWidth)
+  }, [activeIndex]);
+
+  return (
+    <nav className="project-navigation">
+      {tabs.map((tab, index) => {
+        return (
+          <button
+            ref={(el) => (itemsEls.current[index] = el)}
+            onClick={() => {
+              setActiveIndex(index);
+              onChange(tab);
+            }}
+            key={index}
+          >
+            {tab}
+          </button>
+        );
+      })}
+      <span
+        className="indicator"
+        style={{
+          left: `${offset}px`,
+          width: `${indicatorWidth}px`,
+        }}
+      ></span>
+    </nav>
+  );
+}
+
+export default ProjectNavigation;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,37 @@
 import "./Projects.css";
 import { projects } from "../../sources";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProjectsCard from "./ProjectsCard/ProjectsCard";
+import ProjectNavigation from "./ProjectNavigation/ProjectNavigation";
 
 function Projects() {
   const [activeProjects, setActiveProjects] = useState(projects);
+  const [load,setLoad] = useState(false)
+
+  useEffect(
+    ()=>{
+      setLoad(true)
+      setTimeout(() => {
+        setLoad(false)
+      }, 600);
+    }, [activeProjects]
+  )
+  const getTabs = () => {
+    const tabs = ["All"];
+    projects.map((item) => {
+      if (!tabs.includes(item.category)) {
+        tabs.push(item.category);
+      }
+    });
+    return tabs;
+  };
+  const setProjects = (value)=>{
+    if(value==='All'){
+      return setActiveProjects(projects)
+    }
+    const new_projects = projects.filter(item => item.category===value)
+    setActiveProjects(new_projects)
+  }
   return (
     <section id="projects">
       <div className="wrapper">
@@ -13,9 +40,10 @@ function Projects() {
             <span className="gradient-text">projects</span>
           </h1>
         </div>
+        <ProjectNavigation tabs={getTabs()} onChange={setProjects} />
         <div className="projects-container">
           {activeProjects.map((project, index) => {
-            return <ProjectsCard {...project} key={index} />;
+            return <ProjectsCard {...project} key={index} className={load? 'zoom': ''}/>;
           })}
         </div>
       </div>
