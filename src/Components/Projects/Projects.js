c050d6593114d87ad38bb7adc211bebe2e9b9fd0
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2024-12-13 10:00:14 +0300

    completed projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,11 +1,26 @@
-import './Projects.css'
+import "./Projects.css";
+import { projects } from "../../sources";
+import { useState } from "react";
+import ProjectsCard from "./ProjectsCard/ProjectsCard";
 
 function Projects() {
-    return (
-        <h1>
-            Projects
-        </h1>
-    )
+  const [activeProjects, setActiveProjects] = useState(projects);
+  return (
+    <section id="projects">
+      <div className="wrapper">
+        <div className="section-header">
+          <h1 className="heading-1">
+            <span className="gradient-text">projects</span>
+          </h1>
+        </div>
+        <div className="projects-container">
+          {activeProjects.map((project, index) => {
+            return <ProjectsCard {...project} key={index} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
