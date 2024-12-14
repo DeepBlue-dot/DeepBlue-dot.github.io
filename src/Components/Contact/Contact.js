31f27643aa0b92b57b6c898bf5faa4c4edebfc35
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2024-12-14 15:21:44 +0300

    Contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,67 @@
-import './Contact.css'
+import "./Contact.css";
+import { contactOptions } from "../../sources";
 
 function Contact() {
-    return (
-        <h1>
-            Contact
-        </h1>
-    )
+  return (
+    <section id="contact">
+      <div className="wrapper">
+        <div className="contact-options">
+          {contactOptions.map((option, index) => {
+            return (
+              <div className="flex-center option" key={index}>
+                <div className="flex-center icon-wrapper">{option.icon}</div>
+                <h4 className="muted">{option.title}</h4>
+                <h3 className="value">{option.value}</h3>
+              </div>
+            );
+          })}
+        </div>
+        <div className="contact-form">
+          <div className="top">
+            <span className="gradient-text">Join forces with me!</span>
+          </div>
+          <p className="muted">
+            I build with precision and passion, creating web projects that stand
+            out. It's as straightforward as that!
+          </p>
+          <div className="middle">
+            <div className="flex row">
+              <input
+                type="text"
+                placeholder="First name"
+                name="firstname"
+                className="control"
+              />
+              <input
+                type="text"
+                placeholder="Last name"
+                name="lastname"
+                className="control"
+              />
+            </div>
+            <div className="flex row">
+              <input
+                type="email"
+                placeholder="Email name"
+                name="email"
+                className="control"
+              />
+              <input
+                type="tel"
+                placeholder="Phone number"
+                name="phone"
+                className="control"
+              />
+            </div>
+            <textarea name="message" cols={30} rows={10} placeholder="Message" className="control"></textarea>
+          </div>
+          <div className="flex-center bottom">
+            <button className="btn primary">Send Now</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
